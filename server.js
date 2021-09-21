1cb0a35f0d8d2e6e1259d6630038f0f21f924571
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-21 09:49:19 -0500

    server is now connected to mongo

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 'use strict';
 
+require ('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log('It works!');
+});
+
