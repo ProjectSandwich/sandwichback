f57a5f8669fae6b3bce09b163f8d499ad16156a8
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-21 09:57:31 -0500

    express server is working on correct port 3001

--- server.js
+++ server.js
@@ -12,3 +12,15 @@ db.once('open', function() {
   console.log('It works!');
 });
 
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
